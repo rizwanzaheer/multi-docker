c2e2ff662c1bd83aae154dd0c0a86894266c897d
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-09-11 19:14:15 +0500

    adding fib func

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ const redisClient = redis.createClient({
 });
 
 const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
