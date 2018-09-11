f90d93e61d94a680debd993dcc3d18c6049dfce0
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-09-11 21:06:49 +0500

    component fib, calling the apis for data

---
+++ client/src/Fib.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export class Fib extends Component {
+  state = {
+    seenIndexes: [],
+    values: {},
+    index: ''
+  }
+
+  componentDidMount() {
+    this.fetchValues();
+    this.fetchIndexes();
+  }
+
+  async fetchValues() {
+    const values = await axios.get('/api/values/current');
+    this.setState({
+      values: values.data
+    });
+  }
+  async fetchIndexes() {
+    const seenIndexes = await axios.get('/api/values/all');
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Fib;
