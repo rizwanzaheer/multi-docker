734bdf80e8b1dca1b8fa24a0b115ea46aae70d96
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-09-11 20:01:27 +0500

    adding different routes

--- server/index.js
+++ server/index.js
@@ -35,3 +35,27 @@ const redisClient = redis.createClient({
 });
 
 const redisPublisher = redisClient.duplicate();
+
+// express route hanlders
+
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
+  res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post('/values', async (req, res) => {
+  const index = req.body.value;
+
+  if (parseInt(index) > 40) return res.status(422).send('Index too high');
+  redisClient.hset('values', index, 'Nothing yet!');
+});
